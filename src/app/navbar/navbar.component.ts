bb911897b9836e3fd4dbb505ac40b712ef303dbc
jose <joseluisariashc@gmail.com>
2024-04-22 15:13:24 +0200

    update navBar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMagnifyingGlass,faCirclePlus,faInbox,faCalendarDays ,faCalendarCheck,faList} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  faMagnifyingGlass = faMagnifyingGlass;
+  faCirclePlus = faCirclePlus;
+  faInbox = faInbox;
+  faCalendarDays = faCalendarDays;
+  faCalendarCheck = faCalendarCheck;
+  faList = faList;
 }
